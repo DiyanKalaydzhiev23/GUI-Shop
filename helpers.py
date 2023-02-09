78f560e7fdcfc51e298b52c42b3e536447dc5803
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2023-02-09 14:18:58 +0200

    Recreated repo

--- helpers.py
+++
@@ -1,5 +0,0 @@
-from canvas import frame
-
-
-def clean_screen():
-    frame.delete("all")
--- main.py
+++
@@ -1,7 +0,0 @@
-from authentication import render_entry
-from canvas import root
-
-
-if __name__ == '__main__':
-    render_entry()
-    root.mainloop()
